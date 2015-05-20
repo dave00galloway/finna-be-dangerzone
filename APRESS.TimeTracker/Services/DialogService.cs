186c9a7bb14e577b9a100ec5c854b95bd9e19797
Dave Galloway <david.galloway@recordsure.com>
2015-05-20 23:05:05 +0100

    cont.

---
+++ APRESS.TimeTracker/Services/DialogService.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace APRESS.TimeTracker.Services
+{
+    public sealed class DialogService : IDialogService
+    {
+        public void ShowInfo(string title, string message)
+        {
+            MessageBox.Show(title, message, MessageBoxButton.OK);
+        }
+
+        public void ShowError(string title, string message)
+        {
+            MessageBox.Show(title, message, MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        public void ShowAlert(string title, string message)
+        {
+            MessageBox.Show(title, message, MessageBoxButton.OK, MessageBoxImage.Asterisk);
+        }
+
+        public bool AskConfirmation(string title, string message)
+        {
+            return MessageBox.Show(title, message, MessageBoxButton.YesNo, MessageBoxImage.Question) ==
+                   MessageBoxResult.Yes;
+        }
+    }
+}
---
+++ APRESS.TimeTracker/ViewModels/BaseListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace APRESS.TimeTracker.ViewModels
+{
+    public abstract class BaseListViewModel<TEntity> : BaseViewModel<TEntity>
+    {
+        protected BaseListViewModel(List<BaseDetailsViewModel<TEntity>> collection)
+        {
+            Collection = new ObservableCollection<BaseDetailsViewModel<TEntity>>(collection);
+        }
+
+        public ObservableCollection<BaseDetailsViewModel<TEntity>> Collection { get; private set; }
+
+        public BaseDetailsViewModel<TEntity> Selected { get; set; }
+    }
+}
